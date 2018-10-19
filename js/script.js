2cd7af3908a82cb448b16818bd36997492d4ea4f
Bastien Cortequisse <cortequisse.bastien@gmail.com>
2018-10-19 10:25:59 +0200

    a

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function debut () {
+	var bouton=document.getElementById("boutont");
+	bouton.addEventListener("click",tableau);
+  var bouton=document.getElementById("boutonb");
+  bouton.addEventListener("click",bio);
+}
+
+function tableau(){
+	window.location="../ClassificationPeriodique/index.html";
+}
+
+function bio(){
+  window.location="../ClassificationPeriodique/biographie.html";
+}
+
+window.addEventListener("load",debut);
