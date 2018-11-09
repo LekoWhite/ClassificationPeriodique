10c1e80c55e5315db86cf3f550f581b4b422d032
Loucas CUBEDDU <lcubeddu@outlook.fr>
2018-11-09 07:41:38 +0100

    Testing

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+
+    for(var i = 0; i < 118; i++){
+      var atom = document.getElementById('atom' + toString(i));
+      var test_atom = document.getElementById('test_atom');
+      test_atom.innerHTML = xhttp.getElementsByName('nom');
+    }
+
+  }
+};
+xhttp.open("GET", "other/atomes.xml", true);
+xhttp.send();
