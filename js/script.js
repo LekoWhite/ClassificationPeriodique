7b8f0e373eac8292728f4cf94995cd8367b4ad5c
Bastien Cortequisse <cortequisse.bastien@gmail.com>
2018-11-09 09:10:38 +0100

    retirer fonction d'un bouton enlevé

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 function debut () {
 	var bouton=document.getElementById("bouton");
 	bouton.addEventListener("click",tableau);
-<<<<<<< HEAD
-  var bouton=document.getElementById("boutonb");
-  bouton.addEventListener("click",bio);
-}
-=======
-}
 
->>>>>>> 1b07a442916dba37d2b8725586db1f9b3ad2f044
+}
 function tableau(){
 	window.location="../ClassificationPeriodique/index.html";
 }
 
 window.addEventListener("load",debut);
-<<<<<<< HEAD
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -29,5 +22,3 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", "other/atomes.xml", true);
 xhttp.send();
-=======
->>>>>>> 1b07a442916dba37d2b8725586db1f9b3ad2f044
