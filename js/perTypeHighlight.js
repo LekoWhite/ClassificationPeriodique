57817bb2a6fe7e7428fa515e5c7b62ac9eb6cfc4
Loucas CUBEDDU <lcubeddu@outlook.fr>
2018-12-21 10:24:44 +0100

    JS Highlighting Feature Added. Fixed some of the css.

--- js/perTypeHighlight.js
+++ js/perTypeHighlight.js
@@ -1,3 +1,21 @@
-$(document).ready({
-  
+$(document).ready(function(){
+
+  $('.legend_child').mouseover(function(){
+    var atom_type = $(this).attr('class').slice(13);
+    atom_type = '.' + atom_type;
+    var atom_list = $(atom_type);
+    for(let i=0; i < atom_list.length; i++){
+      atom_list[i].classList.add('JSTrigger');
+    }
+  });
+
+  $('.legend_child').mouseout(function(){
+    var atom_type = $(this).attr('class').slice(13, -9);
+    atom_type = '.' + atom_type;
+    var atom_list = $(atom_type);
+    for(let i=0; i < atom_list.length; i++){
+      atom_list[i].classList.remove('JSTrigger');
+    }
+  });
+
 });
